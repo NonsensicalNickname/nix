a7e2133620fe1591bea0cf15207ad4bc0f0a0f53
ceridwen15 <ceridwen@tutamail.com>
2026-08-18 20:16:02 +1000

    cleanup and forward port

--- modules/containers/webserver.nix
+++ modules/containers/webserver.nix
@@ -14,24 +14,18 @@ in
         body = {
             autoStart = true;
             privateNetwork = false;
-            networking.nat = {
-                enable = true;
-                # Use "ve-*" when using nftables instead of iptables
-                internalInterfaces = [ "ve-+" ];
-                externalInterface = "ens3";
-                enableIPv6 = true;
-            };
-            # hostAddress = "192.168.100.10";
-            # localAddress = "192.168.100.11";
-            # hostAddress6 = "fc00::1";
-            # localAddress6 = "fc00::2";
+
+            hostAddress = "192.168.100.10";
+            localAddress = "192.168.100.11";
+            hostAddress6 = "fc00::1";
+            localAddress6 = "fc00::2";
+
             config =
                 {
                     config,
                     ...
                 }:
                 {
-
                     services.httpd = {
                         enable = true;
                         adminAddr = "ceridwen@tutamail.com";
@@ -39,41 +33,25 @@ in
 
                     networking = {
                         firewall.allowedTCPPorts = [ 80 ];
+                        nat = {
+                            enable = true;
+                            internalInterfaces = [ "ve-+" ];
+                            externalInterface = "ens3";
+                            enableIPv6 = true;
+
+                            forwardPorts = [
+                                {
+                                    destination = "192.168.100.11:80";
+                                    sourcePort = 80;
+                                }
+                            ];
+                        };
                     };
 
                     services.unbound.enable = true;
 
                     system.stateVersion = config.system.nixos.release;
                 };
-
-            # autoStart = true;
-            # ephemeral = true;
-            # privateNetwork = false;
-
-            # bindMounts."/var/www/webserver" = {
-            #     hostPath = "/home/ceri/webserver";
-            #     isReadOnly = true;
-            # };
-
-            # hostAddress = "192.168.100.50";
-            # localAddress = "192.168.100.51";
-
-            # config = { containerPkgs, ... }: {
-            #     networking.useDHCP = lib.mkDefault true;
-
-            #     services.httpd = {
-            #         enable = true;
-            #         adminAddr = "ceridwen@tutamail.com";
-            #         # virtualHosts."localhost" = {
-            #         #     documentRoot = "/var/www/webserver";
-            #         #     serverAliases = [ ];
-            #         # };
-            #     };
-
-            #     networking.firewall.allowedTCPPorts = [ 80 ];
-            #     environment.systemPackages = with containerPkgs; [ ];
-            #     system.stateVersion = config.system.nixos.release;
-            # };
         };
     };
 }
