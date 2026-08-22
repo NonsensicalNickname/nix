cc445009444749c9e78b1873bb2a0c6e20a2a757
ceridwen15 <ceridwen@tutamail.com>
2026-08-22 18:03:29 +1000

    README and consistency in host.nix files

--- hosts/knorozov/host.nix
+++ hosts/knorozov/host.nix
@@ -4,20 +4,18 @@
 }:
 {
     imports = [ ./hardware.nix ];
-    config = {
-        system.stateVersion = "26.05";
-
-        modules = {
-            environment.shell = {
-                name = "zsh";
-                package = pkgs.zsh;
-            };
-
-            containers.enabledContainers = [
-                "nginx"
-            ];
-
-            network.dnsResolver = "unbound";
+    config.modules = {
+        environment.shell = {
+            name = "zsh";
+            package = pkgs.zsh;
         };
+
+        containers.enabledContainers = [
+            "nginx"
+        ];
+
+        network.dnsResolver = "unbound";
     };
+
+    config.system.stateVersion = "26.05";
 }
